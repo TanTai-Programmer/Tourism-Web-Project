935a414720277278d1cc6cf1c8a3b6b2a40c1cc1
TanTai-Programmer <tantai.22.04.2003@gmail.com>
2023-04-24 23:07:19 +0700

    Add code JS Map show infomation provide, Thêm JS cho bản đồ hiển thị thông tin du lịch của 1 tỉnh

--- JS/SecondPage.js
+++ JS/SecondPage.js
@@ -1,39 +1,94 @@
+// Khai báo các biến 
 var DaNang =document.querySelector('#VN-DN')
-var Showtest=document.querySelector('#VNSG');
-var html=document.querySelector('.Test__Destination')
-var testAPI ='http://localhost:3000/TP'
-// html.innerHTML='<div>Test</div>'
-var Provide = DaNang.getAttribute("id")
-console.log(Provide)
+var DaNangName = DaNang.getAttribute('class')
+var HCM = document.querySelector('#VN-SG')
+var HCMName = HCM.getAttribute('class')
+var DongNai = document.querySelector('#VN-39')
+var DongNaiName = DongNai.getAttribute('class')
+var BinhThuan = document.querySelector('#VN-40')
+var BinhThuanName = BinhThuan.getAttribute('class')
+var QuangBinh = document.querySelector('#VN-24')
+var QuangBinhName = QuangBinh.getAttribute('class')
 
-DaNang.onmouseover=myfunc
-function myfunc(){
-    fetch(testAPI)
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(callback){
-    gettest(callback)
-  })
-}
-function gettest(a) {
-    var list =a.map(function(a){
-      return `<p>${a} </p>
-      <img src="${a}">`
-    })
-    html.innerHTML = list.join('')
+//In ra màn hình
+var html=document.querySelector('.Content__Destination')
+
+//Lấy dữ liệu API gán 
+var DataAPI ='http://localhost:3000/TP'
+var Gan = ''
+
+fetch(DataAPI)
+.then(function(response){
+  return response.json()
+})
+.then((data) =>{
+  getData(data)
+})
+function getData(data) {
+  DaNang.onmouseover = ()=>{
+    getDataName (DaNangName)
+    const Replace = data.filter(FilterData)
+    const Arg = Replace.map(Printscreen)  //Argument: Đối số 
+    html.innerHTML = Arg.join('')
   }
+  DaNang.onmouseout = Remo
+  BinhThuan.onmouseover = ()=>{
+    getDataName (BinhThuanName)
+    const Replace = data.filter(FilterData)
+    const Arg = Replace.map(Printscreen)  //Argument: Đối số 
+    html.innerHTML = Arg.join('')
+  }
+  BinhThuan.onmouseout = Remo
+  DongNai.onmouseover = ()=>{
+    getDataName (DongNaiName)
+    const Replace = data.filter(FilterData)
+    const Arg = Replace.map(Printscreen)  //Argument: Đối số 
+    html.innerHTML = Arg.join('')
+  }
+  DongNai.onmouseout = Remo
+  QuangBinh.onmouseover = ()=>{
+    getDataName (QuangBinhName)
+    const Replace = data.filter(FilterData)
+    const Arg = Replace.map(Printscreen)  //Argument: Đối số 
+    html.innerHTML = Arg.join('')
+  }
+  QuangBinh.onmouseout = Remo
+  HCM.onmouseover= ()=>{
+    getDataName(HCMName)
+    const Replace = data.filter(FilterData)
+    const Arg=Replace.map(Printscreen)
+    html.innerHTML=Arg.join('')
+  }
+  HCM.onmouseout = Remo
+  function Remo(){
+    html.innerHTML=''
+}
+}
+function FilterData(a){
+  if (a.name === Gan){
+    return a.name === Gan
+  }
+}
+function getDataName (data){
+  Gan = data
+  return Gan
+}
+function Printscreen(b){
+  return `
+  <div class="Content__Destination--image">
+      <img src="${b.img}" alt="" >
+  </div>
+  <p> ${b.name} </p>
+  <p>${b.Description}</p>
+  `
+}
+
+
+
+
 $( document ).on( "mousemove", function( event ) {
-    $( ".Test__Destination" ).css({
+    $( ".Content__Destination" ).css({
         left : event.pageX -200,
         top:event.pageY -600
     } )
   });
-Showtest.onmouseout=Remo
-function Remo(){
-    html.innerHTML=''
-}
-var ShowHN =document.getElementById('VN-40')
-ShowHN.onmouseover=myfunc
-ShowHN.onmouseout=Remo
-DaNang.onmouseout =Remo
