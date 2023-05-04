f01b06886a89de869ef2c8e3771a09a63db9ceba
TanTai-Programmer <tantai.22.04.2003@gmail.com>
2023-05-05 01:34:01 +0700

    Update Sidebar; Add CSS and JS

--- JS/AdminPage.js
+++ JS/AdminPage.js
@@ -4,6 +4,34 @@ var Content = document.querySelector(".Content")
 var ContentNav= document.querySelector(".Content-Nav")
 var btnclose=document.querySelector(".btnclose")
 var btnopen = document.querySelector(".btnopen")
+var SidebarItem = document.querySelectorAll(".Sidebar-List-title")
+var SidebarDown = document.querySelectorAll(".Sidebar-Down")
+$(document).ready(function(){
+    $(".Sidebar-Down1").click(function(){
+      $(".Sidebar-List-Items--Child1").slideToggle();
+      $(".Sidebar-List-Items--Child2").slideUp();
+      $(".Sidebar-List-Items--Child3").slideUp();
+      $(".Sidebar-List-Items--Child4").slideUp();
+    });
+    $(".Sidebar-Down2").click(function(){
+        $(".Sidebar-List-Items--Child2").slideToggle();
+        $(".Sidebar-List-Items--Child1").slideUp();
+        $(".Sidebar-List-Items--Child3").slideUp();
+        $(".Sidebar-List-Items--Child4").slideUp();
+      });
+      $(".Sidebar-Down3").click(function(){
+        $(".Sidebar-List-Items--Child3").slideToggle();
+        $(".Sidebar-List-Items--Child1").slideUp();
+        $(".Sidebar-List-Items--Child2").slideUp();
+        $(".Sidebar-List-Items--Child4").slideUp();
+      });
+      $(".Sidebar-Down4").click(function(){
+        $(".Sidebar-List-Items--Child4").slideToggle();
+        $(".Sidebar-List-Items--Child1").slideUp();
+        $(".Sidebar-List-Items--Child2").slideUp();
+        $(".Sidebar-List-Items--Child3").slideUp();
+      });
+  });
 function Close(){
     Content.style.marginLeft ="5%";
     Content.style.width="100%"
@@ -11,6 +39,13 @@ function Close(){
     sidebar.style.width ="5%";
     btnclose.style.display="none"
     btnopen.style.display="inline"
+    for (let i=0; i<SidebarItem.length; i++){
+        SidebarItem[i].style.display="none"
+    }
+    $(".Sidebar-List-Items--Child1").slideUp();
+    $(".Sidebar-List-Items--Child2").slideUp();
+    $(".Sidebar-List-Items--Child3").slideUp();
+    $(".Sidebar-List-Items--Child4").slideUp();
 }
 function Open(){
     Content.style.marginLeft="15%"
@@ -19,4 +54,8 @@ function Open(){
     sidebar.style.width ="15%";
     btnclose.style.display="inline"
     btnopen.style.display="none"
+    for (let i=0; i<SidebarItem.length; i++){
+        SidebarItem[i].style.display="block"
+        SidebarItem[i].style.overflow="hidden"
+    }
 }
