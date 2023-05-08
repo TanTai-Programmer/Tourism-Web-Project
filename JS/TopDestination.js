71c7ea5443d918d21527eb5058d83982b0caf447
TanTai-Programmer <tantai.22.04.2003@gmail.com>
2023-05-09 00:08:39 +0700

    Add data import function to API, check data before importing into API.Json; Thêm chức năng nhập dữ liệu vào API, kiểm tra dữ liệu trước khi nhập vào API.Json

--- JS/TopDestination.js
+++ JS/TopDestination.js
@@ -67,12 +67,11 @@ const IDinput = document.getElementById("Desination-id")
 const Name = document.getElementById("Destination-name")
 const ImagePath = document.getElementById("Image-Path")
 const Description = document.getElementById("Destination--Description")
+const SubmitForm =document.getElementById("SubmitForm")
+
 
 const APIurl = 'http://localhost:3000/TP'
-AddForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    InputAPI();
-})
+
 function InputAPI(){
         fetch(APIurl,{
             method: 'POST',
@@ -83,75 +82,162 @@ function InputAPI(){
                 id: IDinput.value,
                 name: Name.value,
                 Description: Description.value,
+                img:ImagePath.value,
             })
         })
     }
-    fetch(APIurl)
-    .then(function(response){
-      return response.json()
-    })
-    .then((data) =>{
-      getData(data)
-    })
-    var ShowDemo = document.querySelector('.Content-Container-Data')
-    function getData(data) {
-    }
-
-    const DataDemo = document.querySelector(".Content-Container-Data")
-    const pattenID = /^\d+$/gm
-    var ShowStatus = document.querySelector(".Status")
-        IDinput.addEventListener('blur',()=>{
-            if(IDinput.value===''){
-               console.log("Somthingtsvfdf")
+    $(document).ready(function (){
+    const StatusID = document.querySelector(".StatusID")
+    IDinput.addEventListener('blur', function(e) {
+            CheckID()
+            return CheckID()
+        })
+        function CheckID(){
+            const pattenID = /^\d+$/gm // Đặt biến trong hàm thì hàm chaỵ đúng; ngược lại
+            if((IDinput.value)===''){
+                StatusID.classList.add("Error")
+                IDinput.classList.add("InputError")
+                StatusID.innerHTML ='Input value!'
+                SubmitForm.style.cssText='visibility:hidden;opacity:0'
+                return false;
             }
             else {
                 if(pattenID.test(IDinput.value)){
-                    ShowStatus.style.display="inline-block"
-                    ShowStatus.classList.add("SuccessID")
-                    ShowStatus.classList.remove("ErrorID")
-                    ShowStatus.innerHTML='Success!'
+                    IDinput.classList.add("InputSuccess")
+                    IDinput.classList.remove("InputError")
+                    StatusID.classList.add("Success")
+                    StatusID.classList.remove("Error")
+                    StatusID.innerHTML='*'
+                    SubmitForm.style.cssText='visibility:visible; opacity:1'
                     PrintDemo()
+                    return true
                 }
                  else{
-                    ShowStatus.style.display="inline-block"
-                    ShowStatus.classList.add("ErrorID")
-                    ShowStatus.classList.remove("SuccessID")
-                    ShowStatus.innerHTML='Error Please input true!'
+                    IDinput.classList.add("InputError")
+                    IDinput.classList.remove("InputSuccess")
+                    StatusID.classList.add("Error")
+                    StatusID.classList.remove("Success")
+                    StatusID.innerHTML='Error !! Please input value is string numbers'
+                    SubmitForm.style.cssText='visibility:hidden;opacity:0'
+                    return false
                 }
             }    
-        })
-
-    ImagePath.addEventListener('blur',()=>{
-        if(ImagePath.value===''){
-
+        }
+    const StatusName =document.querySelector(".StatusName")
+    Name.addEventListener('blur', function(e) {
+        CheckName()
+    })
+        function CheckName(){
+            const pattenName = /^[A-Za-z]+$/gm
+            if(Name.value===''){
+                    StatusName.classList.add("Error")
+                    Name.classList.add("InputError")
+                    StatusName.innerHTML ='Input value!'
+                    SubmitForm.style.cssText='visibility:hidden; opacity:0'
+                    return false
             }
             else{
-                PrintDemo()
+                    if(pattenName.test(Name.value)){
+                        Name.classList.add("InputSuccess")
+                        Name.classList.remove("InputError")
+                        StatusName.classList.add("Success")
+                        StatusName.classList.remove("Error")
+                        StatusName.innerHTML='*'
+                        SubmitForm.style.cssText='visibility:visible; opacity:1'
+                        PrintDemo()
+                        return true;
+                    }
+                     else{
+                        Name.classList.add("InputError")
+                        Name.classList.remove("InputSuccess")
+                        StatusName.classList.add("Error")
+                        StatusName.classList.remove("Success")
+                        StatusName.innerHTML='Error !! Please input value is string letter'
+                        SubmitForm.style.cssText='visibility:hidden; opacity:0'
+                        return false
+                    } 
             }
+        }
+    const StatusDesc = document.querySelector(".StatusDescription")
+    Description.addEventListener('blur', function(e){
+        CheckDesc()
     })
-    Description.addEventListener('blur',()=>{
-        if(Description.value===''){
+        function CheckDesc(){
+            if(Description.value==''){
+                Description.classList.add("InputError")
+                Description.classList.remove("InputSuccess")
+                StatusDesc.classList.add("Error")
+                StatusDesc.innerHTML ='Input value!'
+                SubmitForm.style.cssText='visibility:hidden; opacity:0'
+                return false;
+            }
+            else{
+                Description.classList.add("InputSuccess")
+                Description.classList.remove("InputError")
+                StatusDesc.classList.add("Success")
+                StatusDesc.classList.remove("Error")
+                StatusDesc.innerHTML =("*")
+                SubmitForm.style.cssText='visibility:visible; opacity:1'
+                PrintDemo()
+                return true;
+            }
+        }
 
+    const StatusPath = document.querySelector(".StatusPath")
+   ImagePath.addEventListener ('blur', function (e) {
+    CheckPath()
+   })
+        function CheckPath(){
+        const pattenPath = /(^[(..)+(\/)]{3})/gm
+    if (ImagePath.value==''){
+        ImagePath.classList.add("InputError")
+        ImagePath.classList.remove("InputSuccess")
+        StatusPath.classList.add("Error")
+        Name.classList.add("InputError")
+        StatusPath.innerHTML ='Input path'
+        SubmitForm.style.cssText='visibility:hidden; opacity:0'
+        return false;
+    }
+    else{
+        if(pattenPath.test(ImagePath.value)){
+            ImagePath.classList.add("InputSuccess")
+            ImagePath.classList.remove("InputError")
+            StatusPath.classList.add("Success")
+            StatusPath.classList.remove("Error")
+            StatusPath.innerHTML='*'
+            SubmitForm.style.cssText='visibility:visible; opacity:1'
+            PrintDemo()
+            return  true
+        }
+         else{
+            ImagePath.classList.add("InputError")
+            ImagePath.classList.remove("InputSuccess")
+            StatusPath.classList.add("Error")
+            StatusPath.classList.remove("Success")
+            StatusPath.innerHTML='Error !! Please input value is path'
+            SubmitForm.style.cssText='visibility:hidden; opacity:0'
+            return false
+        } 
+    }}
+    AddForm.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        if(CheckID()&&CheckName()&&CheckDesc()&&CheckPath()){
+            console.log(true)
+            InputAPI()
         }
         else{
-            PrintDemo()
+            console.log(false)
         }
-    })
-    Name.addEventListener('blur',()=>{
-        if(Name.value===''){
-
-        }
-        else{
-            PrintDemo()
-        }
-    })
+});
+})
+    const DataDemo = document.querySelector(".Content-Container-Data")
     function PrintDemo() {
         DataDemo.innerHTML = `
         <div class="Content-Container-Data-ID">${IDinput.value}</div>
-                            <img src="${ImagePath.value}" alt="">
-                            <div class="Content-Container-Data-Name">${Name.value}</div>
-                            <div class="Content-Container-Data-Description">${Description.value}</div>
-                            </div>
+        <img src="${ImagePath.value}" alt="">
+        <div class="Content-Container-Data-Name">${Name.value}</div>
+        <div class="Content-Container-Data-Description">${Description.value}</div>
+        </div>
         `
     }
     
