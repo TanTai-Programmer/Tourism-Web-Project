4a301900f0f5040f42a03be34d30616e285a3c80
HoaiTruc <HoaiTruc@example.com>
2023-05-31 00:40:43 +0700

    add code

--- JS/FourthPage.js
+++ JS/FourthPage.js
@@ -67,11 +67,11 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += 1);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideIndex = 1);
 }
 
 function showSlides(n) {
@@ -80,12 +80,12 @@ function showSlides(n) {
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  // for (i = 0; i < slides.length; i++) {
+  //   slides[i].style.display = "none";  
+  // }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  // slides[slideIndex-1].style.display = "block";  
+  // dots[slideIndex-1].className += " active";
 }
